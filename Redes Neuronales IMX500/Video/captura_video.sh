dc71d32aa20f12f98cfcb5f261a9e41d27687bd1
godanitt <danielvazquezlago@gmail.com>
2025-07-29 13:19:09 +0200

    analis video cambios output

--- Redes Neuronales IMX500/Video/captura_video.sh
+++ Redes Neuronales IMX500/Video/captura_video.sh
@@ -4,7 +4,7 @@
 # tiene un argumento (temporal)
 
 INPUT_PATH="Video/raw/Video.mp4"
-OUTPUT_PATH="Video/prediccion/VideoAnalyced.mp4"
+OUTPUT_PATH="Video/prediccion/VideoAnalizado.mp4"
 
 # Captura una imagen (ejemplo usando ffmpeg)
 
@@ -13,7 +13,7 @@ rm Imagenes/raw/*.mp4
 
 
 rpicam-hello --roi 0.45,0.45,0.55,0.55 --preview 7
-rpicam-vid d--roi 0.45,0.45,0.55,0.55  -t 10s -o test.mp4 
+rpicam-vid --roi 0.45,0.45,0.55,0.55  -t 10s -o "$INPUT_PATH" 
 
 
 python3 clasificacion_video.py "$INPUT_PATH" "$OUTPUT_PATH"
