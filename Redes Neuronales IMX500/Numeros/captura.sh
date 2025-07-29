6a6a7f99310d55af1a224f5309817139a5d221ec
godanitt <danielvazquezlago@gmail.com>
2025-07-29 12:13:51 +0200

    red neuronal con picam

--- Redes Neuronales IMX500/Numeros/captura.sh
+++ Redes Neuronales IMX500/Numeros/captura.sh
@@ -12,18 +12,18 @@ rm Imagenes/prediccion/*.jpg
 rm Imagenes/raw/*.jpg
 
 
-rpicam-hello --preview 10 &
+rpicam-hello --roi 0.25,0.25,0.75,0.75 --preview 7
 
 for i in {1}  # Si quieres que sea infinito → while true; do
 do
     # Captura imagen (bloqueante hasta guardar)
-    rpicam-still --output "$INPUT_PATH"
+    rpicam-still --roi 0.25,0.25,0.75,0.75 --output "$INPUT_PATH"
     
     # Procesa la imagen capturada
     python3 clasificacion_imagen.py "$INPUT_PATH" "$OUTPUT_PATH"
     
     # Espera 1 segundo
-    sleep 1
+    sleep 10
 done
 
 # Esperamos a que termine
